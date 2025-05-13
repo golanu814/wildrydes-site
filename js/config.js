1e636b6658ca8620d6861900a0857c394d533bc5
Pranav Gollapudi <149270698+golanu814@users.noreply.github.com>
2025-05-13 12:58:13 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_YkkYX0m6K', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4noavj8r9ktoaeic1smh8lhaeu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'us-east-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
